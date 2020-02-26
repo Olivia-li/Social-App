d7daec22f3ac6b32b4c81999c40ec108c06f517c
Ian Shen <ian891104@berkeley.edu>
2020-02-25 17:30:22 -0800

    created DetailVC, NewSocialVC, setup UI elements without outlets, set up table view in FeedVC

---
+++ IanOliviaSocials/Controller/DetailVC/DetailVC.swift
@@ -0,0 +1,19 @@
+//
+//  DetailVC.swift
+//  IanOliviaSocials
+//
+//  Created by Ian Shen on 2/25/20.
+//  Copyright © 2020 Olivia Li. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+class DetailVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ IanOliviaSocials/Controller/NewSocialVC/NewSocialVC.swift
@@ -0,0 +1,20 @@
+//
+//  NewSocialVC.swift
+//  IanOliviaSocials
+//
+//  Created by Ian Shen on 2/25/20.
+//  Copyright © 2020 Olivia Li. All rights reserved.
+//
+
+
+import Foundation
+import UIKit
+import Firebase
+
+class NewSocialVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
